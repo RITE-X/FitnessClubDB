620f5c4d731d6d3a5828c66cf71a4ef25f625459
RITE-X <re.ritex@gmail.com>
2022-05-23 17:19:26 +0300

    feat: add routed command add routed command class for runtime command creation

---
+++ Commands/RoutedCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Input;
+using FitnessClubDB.Commands.Base;
+
+namespace FitnessClubDB.Commands;
+
+public class RoutedCommand : Command
+{
+    private readonly Action<object> _execute;
+    private readonly Func<object, bool>? _canExecute;
+    
+    public RoutedCommand(Action<object> execute, Func<object, bool>? canExecute = null)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+
+    public override bool CanExecute(object parameter) => _canExecute is null || _canExecute(parameter);
+
+    public override void Execute(object parameter) => _execute(parameter);
+}
