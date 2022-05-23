2fb83d09041b599cba31019050ff179f085f1485
RITE-X <re.ritex@gmail.com>
2022-05-23 17:16:28 +0300

    feat: add database entry point add root class for access to database

---
+++ Models/Root.cs
@@ -0,0 +1,25 @@
+﻿using FitnessClubDB.Models.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessClubDB.Models;
+
+public static class Root
+{
+     public static FitnessClubContext FitnessClubContext { get; }
+
+      static Root()
+      {
+          FitnessClubContext =
+              new FitnessClubContext("Server=(localdb)\\mssqllocaldb;Database=FitnessClub;Trusted_Connection=True;");
+              
+          FitnessClubContext.Cheques.Load();
+          FitnessClubContext.Clients.Load();
+          FitnessClubContext.Complexes.Load();
+          FitnessClubContext.Memberships.Load();
+          FitnessClubContext.Trainers.Load();
+          FitnessClubContext.Visits.Load();
+          FitnessClubContext.Workouts.Load();
+
+          
+      }
+}
