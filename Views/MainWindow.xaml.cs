81ace111eb7960737f71c39fae8626bc81116f14
RITE-X <re.ritex@gmail.com>
2022-05-23 17:32:43 +0300

    feat: implement database binding - implement database binding using xaml binding - move the main application window to the views folder

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using FitnessClubDB.Models;
+using FitnessClubDB.Models.Database;
+
+namespace FitnessClubDB.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            var s = Trainers;
+        }
+        
+
+        private void OnAutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+            => e.Column.Header = ((PropertyDescriptor)e.PropertyDescriptor)?.DisplayName ?? e.Column.Header;
+
+        // private void Home_OnRowEditEnding(object? sender, DataGridRowEditEndingEventArgs e)
+        // {
+        //   e
+        // }
+
+
+    }
+}
