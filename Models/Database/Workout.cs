7f47ac0d57f4d060e4b1ccf4bf40a128a0d49118
RITE-X <re.ritex@gmail.com>
2022-05-27 20:14:08 +0300

    style: fix style issues

--- Models/Database/Workout.cs
+++ Models/Database/Workout.cs
@@ -8,6 +8,7 @@ namespace FitnessClubDB.Models.Database
         public int ClientId { get; set; }
         public int TrainerId { get; set; }
         public DateTime StartTime { get; set; }
+
         public virtual Client Client { get; set; } = null!;
         public virtual Trainer Trainer { get; set; } = null!;
     }
