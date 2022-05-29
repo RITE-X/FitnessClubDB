ca5bfa6dff79bbe02ea0615e5b7d63edd8e318c7
RITE-X <re.ritex@gmail.com>
2022-05-29 21:04:20 +0300

    feat: add register client window

---
+++ ViewModels/RegisterNewClientViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using FitnessClubDB.Commands;
+using FitnessClubDB.Models;
+using FitnessClubDB.Models.Database;
+using FitnessClubDB.Services.WindowCloser;
+using FitnessClubDB.ViewModels.Base;
+
+namespace FitnessClubDB.ViewModels;
+
+public class RegisterNewClientViewModel : ViewModel, ICloseWindow
+{
+    private string? _firstName;
+
+    public string? FirstName
+    {
+        get => _firstName;
+        set => Set(ref _firstName, value);
+    }
+
+    private string? _lastName;
+
+    public string? LastName
+    {
+        get => _lastName;
+        set => Set(ref _lastName, value);
+    }
+
+    private string? _middleName;
+
+
+    public string? MiddleName
+    {
+        get => _middleName;
+        set => Set(ref _middleName, value);
+    }
+
+    public BindingList<Trainer> Trainers => DBRoot.GetContext().Trainers.Local.ToBindingList();
+
+
+    private Trainer? _selectedTrainer;
+
+    public Trainer? SelectedTrainer
+    {
+        get => _selectedTrainer;
+        set => Set(ref _selectedTrainer, value);
+    }
+
+    public BindingList<Membership> Memberships => DBRoot.GetContext().Memberships.Local.ToBindingList();
+
+    private Membership? _selectedMembership;
+
+    public Membership? SelectedMembership
+    {
+        get => _selectedMembership;
+        set => Set(ref _selectedMembership, value);
+    }
+
+    private RoutedCommand? _registerNewClient;
+
+    public RoutedCommand? RegisterNewClient => _registerNewClient ??= new RoutedCommand(o =>
+    {
+        DBRoot.GetContext().Clients.Local.Add(new Client
+            {FirstName = FirstName, LastName = LastName, MiddleName = MiddleName, Trainer = SelectedTrainer});
+
+
+        DBRoot.GetContext().Cheques.Local.Add(new Cheque
+        {
+            ClientId = DBRoot.GetContext().Clients.Local.Last().Id, PurchaseDate = DateTime.Now,
+            Service = SelectedMembership
+        });
+
+        DBRoot.GetContext().SaveChanges();
+
+        Close?.Invoke();
+    }, o => !string.IsNullOrEmpty(FirstName)  && !string.IsNullOrEmpty(LastName) && SelectedTrainer != null && SelectedMembership != null);
+
+    public Action? Close { get; set; }
+    public bool CanClose() => true;
+}
---
+++ Views/ServiceWindows/RegisterNewClientWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using FitnessClubDB.ViewModels;
+
+namespace FitnessClubDB.Views.ServiceWindows;
+
+public partial class RegisterNewClientWindow : Window
+{
+    public RegisterNewClientWindow(RegisterNewClientViewModel viewModel)
+    {
+        Owner = Application.Current.MainWindow;
+        DataContext = viewModel;
+        InitializeComponent();
+    }
+
+}
