d05875e1f232ea0e06e06ca3abc7ca7b9e3b5d4c
RITE-X <re.ritex@gmail.com>
2022-05-29 21:09:59 +0300

    feat: add training registration window

---
+++ ViewModels/TrainerRecordViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using FitnessClubDB.Commands;
+using FitnessClubDB.Models;
+using FitnessClubDB.Models.Database;
+using FitnessClubDB.Services.WindowCloser;
+using FitnessClubDB.ViewModels.Base;
+
+namespace FitnessClubDB.ViewModels;
+
+public class TrainerRecordViewModel : ViewModel, ICloseWindow
+{
+    private readonly Client _client;
+
+    public TrainerRecordViewModel(Client client)
+    {
+        _client = client;
+    }
+
+    public BindingList<Specialization> Specializations => DBRoot.GetContext().Specializations.Local.ToBindingList();
+
+    private Trainer? _selectedTrainer;
+
+    public Trainer? SelectedTrainer
+    {
+        get => _selectedTrainer;
+        set => Set(ref _selectedTrainer, value);
+    }
+
+    private Specialization? _selectedSpecialization;
+
+    public Specialization? SelectedSpecialization
+    {
+        get => _selectedSpecialization;
+        set => Set(ref _selectedSpecialization, value);
+    }
+
+    private DateTime _startTime;
+
+    public DateTime StartTime
+    {
+        get => _startTime;
+        set => Set(ref _startTime, value);
+    }
+
+
+    private RoutedCommand? _insertWorkout;
+
+    public RoutedCommand InsertWorkout => _insertWorkout ??= new RoutedCommand(_ =>
+    {
+        DBRoot.GetContext().Workouts.Local.Add(new Workout
+            {ClientId = _client.Id, StartTime = StartTime, Trainer = SelectedTrainer});
+
+        DBRoot.GetContext().SaveChanges();
+        Close?.Invoke();
+    }, _ => SelectedTrainer != null && SelectedSpecialization != null && StartTime > DateTime.Now);
+
+    public Action? Close { get; set; }
+
+    public bool CanClose() => true;
+}
---
+++ Views/ServiceWindows/TrainerRecordWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using FitnessClubDB.ViewModels;
+
+namespace FitnessClubDB.Views.ServiceWindows;
+
+public partial class TrainerRecordWindow : Window
+{
+    public TrainerRecordWindow(TrainerRecordViewModel trainerRecordViewModel)
+    {
+        Owner = Application.Current.MainWindow;
+        DataContext = trainerRecordViewModel;
+        InitializeComponent();
+    }
+}
