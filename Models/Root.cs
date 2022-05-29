fab033e6f6d392aa808205b198c72dd4637cc8a3
RITE-X <re.ritex@gmail.com>
2022-05-29 21:00:47 +0300

    refactor: rename Root class to BDRoot  -add Specializations table loader  -add GetContext method

--- Models/Root.cs
+++ Models/Root.cs
@@ -3,23 +3,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FitnessClubDB.Models;
 
-public static class Root
+public static class DBRoot
 {
-     public static FitnessClubContext FitnessClubContext { get; }
+    private static readonly FitnessClubContext FitnessClubContext;
 
-      static Root()
-      {
-          FitnessClubContext =
-              new FitnessClubContext("Server=(localdb)\\mssqllocaldb;Database=FitnessClub;Trusted_Connection=True;");
-              
-          FitnessClubContext.Cheques.Load();
-          FitnessClubContext.Clients.Load();
-          FitnessClubContext.Complexes.Load();
-          FitnessClubContext.Memberships.Load();
-          FitnessClubContext.Trainers.Load();
-          FitnessClubContext.Visits.Load();
-          FitnessClubContext.Workouts.Load();
+    static DBRoot()
+    {
+        FitnessClubContext =
+            new FitnessClubContext("Server=(localdb)\\mssqllocaldb;Database=FitnessClub;Trusted_Connection=True;");
 
-          
-      }
+        FitnessClubContext.Cheques.Load();
+        FitnessClubContext.Clients.Load();
+        FitnessClubContext.Complexes.Load();
+        FitnessClubContext.Memberships.Load();
+        FitnessClubContext.Trainers.Load();
+        FitnessClubContext.Visits.Load();
+        FitnessClubContext.Workouts.Load();
+        FitnessClubContext.Specializations.Load();
+    }
+
+    public static FitnessClubContext GetContext()
+    {
+        return FitnessClubContext;
+    }
 }
